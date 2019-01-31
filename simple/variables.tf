53b57bc0618343b4a33ae6cc1a9cf8e930e2edbb
Andy Day <andy@andy-day.com>
2019-01-31 13:41:49 -0800

    Update variables.tf

--- simple/variables.tf
+++ simple/variables.tf
@@ -1,21 +1,9 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# Environmental variables
-# You probably want to define these as environmental variables.
-# Instructions on that are here: https://github.com/cloud-partners/oci-prerequisites
-# ---------------------------------------------------------------------------------------------------------------------
-
-# Required by the OCI Provider
 variable "compartment_id" {}
 variable "region" {}
 
 # Key used to SSH to OCI VMs
 variable "ssh_public_key" {}
 variable "ssh_private_key" {}
-
-# ---------------------------------------------------------------------------------------------------------------------
-# Optional variables
-# The defaults here will give you a cluster.  You can also modify these.
-# ---------------------------------------------------------------------------------------------------------------------
 
 variable "dse" {
   type = "map"
